5c9ed74831e57f83c90e0c6e5e7079b23b08e093
Rusianeitor <jeffams1234@gmail.com>
2023-12-29 21:31:50 -0500

    Se actualiza el proyecto 10/34

--- src/test/java/ReqResTests.java
+++ src/test/java/ReqResTests.java
@@ -1,13 +1,12 @@
-import io.restassured.RestAssured;
-import io.restassured.builder.RequestSpecBuilder;
-import io.restassured.filter.log.RequestLoggingFilter;
-import io.restassured.filter.log.ResponseLoggingFilter;
-import io.restassured.http.ContentType;
-
+import io.restassured.response.Response;
 import org.apache.http.HttpStatus;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import restUtils.RestUtils;
+import utils.JsonUtils;
+
+import java.io.IOException;
+import java.util.Map;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.*;
@@ -15,37 +14,48 @@ import static org.hamcrest.MatcherAssert.*;
 
 public class ReqResTests {
 
-    @BeforeEach
-    public void setup(){
-        RestAssured.baseURI="https://reqres.in";
-        RestAssured.basePath="/api";
+    /*@BeforeEach
+    public void setup() {
+        RestAssured.baseURI = "https://reqres.in";
+        RestAssured.basePath = "/api";
         RestAssured.filters(new RequestLoggingFilter(), new ResponseLoggingFilter());
-        RestAssured.requestSpecification=new RequestSpecBuilder()
+        RestAssured.requestSpecification = new RequestSpecBuilder()
                 .setContentType(ContentType.JSON)
                 .build();
-    }
+    }*/
 
     @Test
-    public void loginTest(){
+    public void loginTest() {
         given()
                 .body("{\"email\": \"eve.holt@reqres.in\",\"password\": \"cityslicka\"}")
                 .post("login")
                 .then()
                 .statusCode(200)
-                .body("token",notNullValue());
+                .body("token", notNullValue());
     }
 
     @Test
-    public void getSingleUserTest(){
+    public void getSingleUserTest() {
         given()
                 .get("users/2")
                 .then()
                 .statusCode(HttpStatus.SC_OK) //Revisar cada uno, SC_OK es 200
-                .body("data.id",equalTo(2));
+                .body("data.id", equalTo(2));
     }
 
     @Test
-    public void deleteUserTest(){
+    public void createUserTest() throws IOException {
+        String env = System.getProperty("env") == null ? "qa" : System.getProperty("env");
+        Map<String, String> data = JsonUtils.getJsonDataAsMap("users/"+env+"/usersApiData.json");
+        String endpoint = data.get("createUsersEndpoint");
+        //String payload = Payloads.getCreateUserPayloadFromString("Jeff", "QA Trainee");
+        Map<String, Object> payload = Payloads.getCreateUserPayloadFromMap("Jeff", "QA Trainee");
+        Response response = RestUtils.performPost(endpoint, payload);
+        Assertions.assertEquals(response.statusCode(), 201);
+    }
+
+    @Test
+    public void deleteUserTest() {
         given()
                 .delete("users/2")
                 .then()
@@ -53,27 +63,27 @@ public class ReqResTests {
     }
 
     @Test
-    public void patchUserTest(){
+    public void patchUserTest() {
         given()
                 .body("{\"name\": \"jeff\",\"job\": \"zion resident\"}")
                 .patch("user/2")
                 .then()
                 .statusCode(HttpStatus.SC_OK)
-                .body("name",containsString("jeff"));
+                .body("name", containsString("jeff"));
     }
 
     @Test
-    public void putUserTest(){
+    public void putUserTest() {
         String nameUpdated = given()
                 .body("{\"name\": \"jeff\",\"job\": \"qa\"}")
                 .put("users/2")
                 .then()
                 .statusCode(HttpStatus.SC_OK)
-                .body("name",equalTo("jeff"))
+                .body("name", equalTo("jeff"))
                 .extract()
                 .jsonPath()
                 .getString("name");
 
-        assertThat(nameUpdated,equalTo("jeff"));
+        assertThat(nameUpdated, equalTo("jeff"));
     }
 }
